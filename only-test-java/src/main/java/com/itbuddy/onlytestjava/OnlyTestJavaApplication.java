c6712ac49eff558c9d757ff70534a5ebe22ff92b
나평근 <skvudrms54@gmail.com>
2021-02-19 00:27:01 +0900

    Stream 정렬 추가

---
+++ only-test-java/src/main/java/com/itbuddy/onlytestjava/OnlyTestJavaApplication.java
@@ -0,0 +1,14 @@
+package com.itbuddy.onlytestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OnlyTestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyTestJavaApplication.class, args);
+	}
+
+}
---
+++ only-test-java/src/test/java/com/itbuddy/onlytestjava/OnlyTestJavaApplicationTests.java
@@ -0,0 +1,70 @@
+package com.itbuddy.onlytestjava;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class OnlyTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		List<Item> mapArrayList = new ArrayList<Item>();
+		mapArrayList.add(new Item(1, "text"));
+		mapArrayList.add(new Item(2, "text"));
+		mapArrayList.add(new Item(3, "text"));
+		mapArrayList.add(new Item(9, "text"));
+		mapArrayList.add(new Item(8, "text"));
+		mapArrayList.add(new Item(7, "text"));
+
+		mapArrayList
+		.stream()
+		.forEach(System.out::println);
+		
+		System.out.println("\n\n\n");
+
+		//스트림으로  sort
+		List<Item> sortedmapArrayList = mapArrayList
+											.stream()
+											.sorted(Comparator.comparing(Item::getKey))
+											.collect(Collectors.toCollection(ArrayList<Item>::new));
+
+		sortedmapArrayList.stream().forEach(System.out::println);
+
+	}
+}
+
+class Item{
+	Integer key = null;
+	String text = null;
+
+	@Override
+	public String toString() {
+		return "( key : "+ key + ", text : " + text + " )";
+	}
+	public Integer getKey() {
+		return key;
+	}
+	public void setKey(Integer key) {
+		this.key = key;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Item(Integer key, String text){
+		this.key = key;
+		this.text = text;
+	}
+}
